4d2d1593ac4e6f883ec2152943e72c9af93c3083
Herivahatra <h.ranaivoarson@etechconsulting-mg.com>
2022-10-28 15:02:42 +0300

    [IMPL] Init modules structure

---
+++ choreograph-addons/choreograph_base/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'choreograph base',
+    'summary': 'choreograph base module',
+    'sequence': 10,
+    'description': "",
+    'category': '',
+    'website': '',
+    'images': [],
+    'depends': [],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': False,
+    'assets': {
+        'web._assets_primary_variables': [],
+        'web.assets_backend': [],
+        'web.assets_frontend': [],
+    },
+}
