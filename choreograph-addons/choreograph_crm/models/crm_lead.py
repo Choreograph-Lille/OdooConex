4bc98c6048b7cecfd1fabf861acc8cf70351e385
Andriantsilavo RAMAMONJISOA <a.ramamonjisoa@etech.local>
2022-11-25 14:20:37 +0300

    [add] CNXMIG-23: add crm fields and rule on SO creation
    [fix] CNXMIG-23: fix dependecies, add sale_crm

--- choreograph-addons/choreograph_crm/models/crm_lead.py
+++ choreograph-addons/choreograph_crm/models/crm_lead.py
@@ -7,6 +7,18 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     sox = fields.Boolean('SOX')
-    activity_sector = fields.Char('Activity sector')
-    category_name = fields.Char('Category name')
+    activity_sector = fields.Selection([
+        ('press', 'Press'),
+        ('charity', 'Charity'),
+        ('bank', 'Bank / Insurance'),
+        ('leisure', 'Leisure'),
+    ], string='Activity sector', related='partner_id.activity')
+    category_name = fields.Char('Category name', related='partner_id.category_name')
+    agency_id = fields.Many2one('res.partner', string='Agency')
 
+    def action_new_quotation(self):
+        action = self.env["ir.actions.actions"]._for_xml_id("sale_crm.sale_action_quotations_new")
+        action['context'] = self._prepare_opportunity_quotation_context()
+        action['context']['search_default_opportunity_id'] = self.id
+        action['context']['default_partner_invoice_id'] = self.agency_id.id
+        return action
