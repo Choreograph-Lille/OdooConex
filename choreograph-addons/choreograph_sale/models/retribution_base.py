cc5b9dfdf42070d0485b057dec3d16ce949d9fb2
Andriantsilavo RAMAMONJISOA <a.ramamonjisoa@etech.local>
2022-12-16 14:23:10 +0300

    [fix] CNXMIG-38: add retribution calcul

--- choreograph-addons/choreograph_sale/models/retribution_base.py
+++ choreograph-addons/choreograph_sale/models/retribution_base.py
@@ -10,8 +10,16 @@ class RetributionBase(models.Model):
     name = fields.Char('Name', required=True)
     is_open = fields.Boolean('Open')
     is_multi_base = fields.Boolean('Multi-base')
-    retribution_rate = fields.Float('Retribution rate')
+    retribution_rate = fields.Float('Retribution rate', compute='_compute_retribution_rate')
     quota_base_ids = fields.One2many('retribution.base.line', 'multi_base_id')
+
+    @api.depends('quota_base_ids', 'is_multi_base')
+    def _compute_retribution_rate(self):
+        for rec in self:
+            if rec.is_multi_base:
+                rec.retribution_rate = sum([qb.volume_percentage * qb.retribution_percentage for qb in rec.quota_base_ids])/100
+            else:
+                rec.retribution_rate = 0
 
 
 class RetributionBaseLine(models.Model):
