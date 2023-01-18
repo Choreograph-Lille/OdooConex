a97d24de8c258b5e119f3e9002eb49ca2ca04591
RAVALISON A. Tsiorimampionina <t.ravalison@etechconsulting-mg.com>
2023-01-18 14:37:39 +0300

    [ADD] Added choreograph_sox module for managing access rights. Creating user profiles with the res.groups object

---
+++ choreograph-addons/choreograph_sox/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Choreograph Sox : User rights management',
+    'version': '16.0.0.1',
+    'license': 'LGPL-3',
+    'category': 'Tools',
+    'summary': 'Management of user rights, creation of user profiles',
+    'sequence': -96,
+    'author': 'ArkeUp',
+    'website': 'https://arkeup.com',
+    'depends': [
+        'purchase',
+        'sales_team',
+        'project',
+        'hr_expense',
+        'documents',
+        'mass_mailing'
+    ],
+    'data': [
+        'data/ir_module_category.xml',
+        'security/res_groups.xml'
+    ],
+    'installable': True,
+    'application': False,
+}
