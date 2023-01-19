4d387ebe6a55fb6f615e86223c4bc7c44201d54b
RAVALISON A. Tsiorimampionina <t.ravalison@etechconsulting-mg.com>
2023-01-19 16:36:13 +0300

    [REF] Correction indentation
    [FIX]
    Correction precommit pour ne pas prendre en compte le __manifest__
    Ajout exclusive_user sur l'enregistrement studio.approval.rule

--- choreograph-addons/choreograph_purchase/__manifest__.py
+++ choreograph-addons/choreograph_purchase/__manifest__.py
@@ -1,14 +1,28 @@
+# -*- coding: utf-8 -*-
 {
-    "name": "Choreograph Purchase",
-    "version": "16.0.0.0.1",
-    "license": "LGPL-3",
-    "category": "Purchase",
-    "sequence": -89,
-    "summary": "Manage Choreograph Purchase",
-    "author": "ArkeUp",
-    "website": "https://arkeup.com",
-    "depends": ["purchase", "web_studio", "choreograph_base", "choreograph_sox"],
-    "data": ["data/studio_approval_rule.xml"],
-    "installable": True,
-    "application": False,
+    'name': 'Choreograph Purchase',
+    'version': '16.0.0.1',
+    'license': 'LGPL-3',
+    'category': 'Purchase',
+    'sequence': -89,
+    'summary': 'Manage Choreograph Purchase',
+    'description': """This module allows to install specific purchase features""",
+    'author': 'ArkeUp',
+    'website': 'https://arkeup.com',
+    'depends': [
+        'purchase',
+        'web_studio',
+        'choreograph_base',
+        'choreograph_sox'
+    ],
+    'data': [
+        'data/studio_approval_rule.xml'
+    ],
+    'assets': {
+        'web._assets_primary_variables': [],
+        'web.assets_backend': [],
+        'web.assets_frontend': [],
+    },
+    'installable': True,
+    'application': False,
 }
