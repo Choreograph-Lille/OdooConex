c4b1628e48d6bda2d6d993855aee10d76736f915
RAVALISON A. Tsiorimampionina <120464786+tsiorimgs@users.noreply.github.com>
2023-02-02 10:30:51 +0300

    Develop (#93)
    * [fix] CNXMIG-18: add translation
    
    * [FIX]
    -  Changement res.groups en res.user.role
    -  Changement groupe studio.approval.rule pour correspondre au group géneré par res.user.role
    -  Suppression des roles déjà présent dans maas_base
    
    * [FIX] (#90)
    
    - Ajout attribut primary="True" sur le template maas_website.lgin
    - Changement route pour la fonction web_login > /web/bo/login
    - héritage web.login pour changer les attributs de t-attf-action et onsubmit pour prendre en compte l'url /web/bo/login
    - Création de fonction pour la route /web/login
    - Suppression template qui cache le header et footer du site web
    
    * [add] CNXMIG-20: add fields in task form view
    
    ---------
    
    Co-authored-by: Andriantsilavo RAMAMONJISOA <a.ramamonjisoa@etech.local>

--- choreograph-addons/choreograph_sale_project/models/project_task.py
+++ choreograph-addons/choreograph_sale_project/models/project_task.py
@@ -1,6 +1,6 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
-TASK_NUMBER = [(str(n), str(n)) for n in range(5, 100, 5)]
+from odoo.addons.choreograph_sale.models.operation_condition import TASK_NUMBER
 
 
 class ProjectTask(models.Model):
@@ -9,3 +9,68 @@ class ProjectTask(models.Model):
     task_type_id = fields.Many2one('choreograph.project.task.type', string='Task type')
     task_number = fields.Selection(TASK_NUMBER)
     is_template = fields.Boolean(related='project_id.is_template')
+    catalogue_ids = fields.Many2many('res.partner.catalogue', related='sale_order_id.catalogue_ids')
+    related_base = fields.Many2one('retribution.base', related='sale_order_id.related_base')
+    user_id = fields.Many2one('res.users', related='sale_order_id.user_id')
+
+    id_title = fields.Char('ID Title', related='partner_id.ref')
+    note = fields.Text('Information')
+    campaign_file_name = fields.Char('File Name')
+    bat_from = fields.Char('From', related='sale_order_id.bat_from')
+    bat_internal = fields.Char(related='sale_order_id.bat_internal')
+    bat_client = fields.Char(related='sale_order_id.bat_client')
+    bat_comment = fields.Text('BAT Comment', related='sale_order_id.bat_comment')
+    excluded_provider = fields.Char()
+    optout_comment = fields.Text()
+    witness_file_name = fields.Char('File Name', related='sale_order_id.witness_file_name')
+    witness_comment = fields.Text(related='sale_order_id.witness_comment')
+    file_name = fields.Char()
+    file_quantity = fields.Char()
+    volume = fields.Float()
+    dedup_title_number = fields.Char()
+    family_conex = fields.Boolean()
+    provider_file_name = fields.Char()
+    provider_delivery_address = fields.Char('Delivery Address')
+    provider_comment = fields.Text()
+    desired_finished_volume = fields.Char(related='sale_order_id.desired_finished_volume')
+    start_date = fields.Date()
+    routing_base = fields.Char()
+    specific_counting = fields.Text()
+    send_with = fields.Char()
+    deposit_date_1 = fields.Date()
+    deposit_date_2 = fields.Date()
+    deposit_date_3 = fields.Date()
+
+    is_info_validated = fields.Boolean('Infos Validated')
+    po_livedata_number = fields.Integer('PO Livedata Number')
+    campaign_name = fields.Char()
+    reception_date = fields.Date(related='sale_order_id.reception_date')
+    reception_location = fields.Char('Where to find ?', related='sale_order_id.reception_location')
+    personalization = fields.Boolean(related='sale_order_id.personalization')
+    routing_date = fields.Date(related='sale_order_id.routing_date')
+    routing_end_date = fields.Date(related='sale_order_id.routing_end_date')
+    campaign_type = fields.Char()
+    volume_detail = fields.Text()
+    sender = fields.Char(related='sale_order_id.sender')
+    quantity_to_deliver = fields.Integer()
+    to_validate = fields.Integer()
+    object = fields.Char()
+    ab_test = fields.Boolean('A/B Test')
+    is_preheader_available = fields.Boolean('Preheader available in HTML')
+    comment = fields.Text(related='sale_order_id.comment')
+    bat_desired_date = fields.Date()
+    folder_key = fields.Char()
+
+    segment_ids = fields.Many2many('operation.segment', compute='compute_segment_ids')
+    operation_condition_ids = fields.Many2many('operation.condition', compute='compute_operation_condition_ids')
+
+    @api.depends('sale_order_id', 'sale_order_id.segment_ids', 'sale_order_id.repatriate_information')
+    def compute_segment_ids(self):
+        if self.sale_order_id.repatriate_information:
+            self.segment_ids = [(6, 0, self.env['operation.segment'].search([('order_id', '=', self.sale_order_id.id)]).ids)]
+        else:
+            self.segment_ids = False
+
+    @api.depends('sale_order_id', 'sale_order_id.operation_condition_ids')
+    def compute_operation_condition_ids(self):
+        self.operation_condition_ids = [(6, 0, self.env['operation.condition'].search([('order_id', '=', self.sale_order_id.id)]).ids)]
