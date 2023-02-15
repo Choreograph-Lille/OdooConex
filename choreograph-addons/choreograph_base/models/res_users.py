cb084dc33010065f21ca3703e94adc9d26ef3766
Omar OUHARI <omar.ouhari@gmail.com>
2023-02-15 15:12:36 +0100

    [IMPL] #CNXMIG-2085: create api to execute select query

---
+++ choreograph-addons/choreograph_base/models/res_users.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from .tools import clean_dict
+from odoo import api, fields, models
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    def execute_query(self, query=None):
+        if not query:
+            return False
+        self._cr.execute(query)
+        result = []
+        for data in self._cr.dictfetchall():
+            result.append(clean_dict(data))
+        return result
