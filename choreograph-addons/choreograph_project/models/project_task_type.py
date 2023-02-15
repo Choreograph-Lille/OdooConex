2a2ca17aed5ce3bbe32f40f2fe81f578712ffb67
RAVALISON A. Tsiorimampionina <t.ravalison@etechconsulting-mg.com>
2023-02-15 16:44:16 +0300

    [ADD] Création menu Opération Création project_operation_type pour projet, tâches, étapes de projet, étapes des tâches. Calcule project_operation_type pour les projets qui ont un sale_order_id => "operation" Copie des menus : project, mes taches, analyse, configuration/projets, configuration/étapes de projet, configuration/étapes des tâches : avec domaine et contexte correspondant au menu parent (opération / projet)

--- choreograph-addons/choreograph_project/models/project_task_type.py
+++ choreograph-addons/choreograph_project/models/project_task_type.py
@@ -1,8 +1,10 @@
-from odoo import fields, models
 from .project_project_stage import STAGE_NUMBER
+from .project_project import PROJECT_OPERATION_TYPE
+from odoo import fields, models
 
 
 class ProjectTaskType(models.Model):
     _inherit = 'project.task.type'
 
     stage_number = fields.Selection(STAGE_NUMBER)
+    project_operation_type = fields.Selection(PROJECT_OPERATION_TYPE, default='standard', required=True)
