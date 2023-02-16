b5bdfdebe1e1af23053eff50d1a50a1f315096c0
RAVALISON A. Tsiorimampionina <120464786+tsiorimgs@users.noreply.github.com>
2023-02-16 15:42:42 +0300

    Revert "Develop (#151)"
    This reverts commit f06057b557e7e2d3aa305b0c0f5c3d4b08585a3d.

--- choreograph-addons/choreograph_project/report/project_report.py
+++ choreograph-addons/choreograph_project/report/project_report.py
@@ -1,14 +1,14 @@
 from odoo import fields, models
-from ..models.project_project import TYPE_OF_PROJECT
+from ..models.project_project import PROJECT_OPERATION_TYPE
 
 
 class ProjectReport(models.Model):
     _inherit = 'report.project.task.user'
 
-    type_of_project = fields.Selection(TYPE_OF_PROJECT, default='standard', required=True)
+    project_operation_type = fields.Selection(PROJECT_OPERATION_TYPE, default='standard', required=True)
 
     def _select(self):
-        return super()._select() + ", t.type_of_project as type_of_project"
+        return super()._select() + ", t.project_operation_type as project_operation_type"
 
     def _group_by(self):
-        return super()._group_by() + ", t.type_of_project"
+        return super()._group_by() + ", t.project_operation_type"
