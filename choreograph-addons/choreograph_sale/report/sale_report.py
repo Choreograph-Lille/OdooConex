5f315b140570eb5838ed8cc3a352e085e6112e00
RAVALISON A. Tsiorimampionina <t.ravalison@etechconsulting-mg.com>
2023-04-17 13:59:39 +0300

    [IMP] features/HT01027 edit :         choreograph-addons/choreograph_sale/i18n/fr.po edit :         choreograph-addons/choreograph_sale/report/sale_report.py

--- choreograph-addons/choreograph_sale/report/sale_report.py
+++ choreograph-addons/choreograph_sale/report/sale_report.py
@@ -5,13 +5,27 @@ class SaleReport(models.Model):
     _inherit = 'sale.report'
 
     retribution = fields.Float(readonly=True)
+    state_specific = fields.Selection([
+        ('forecast', 'Forecast'),
+        ('lead', 'Lead'),
+        ('prospecting', 'Prospecting'),
+        ('qualif', 'Qualif'),
+        ('draft', 'Quotation'),
+        ('sent', 'Quotation Sent'),
+        ('sale', 'Sales Order'),
+        ('done', 'Locked'),
+        ('cancel', 'Cancelled'),
+    ], default='forecast')
 
     def _select_additional_fields(self):
         res = super()._select_additional_fields()
-        res['retribution'] = "l.retribution_cost"
+        res.update({
+            'retribution': 'l.retribution_cost',
+            'state_specific': 's.state_specific',
+        })
         return res
 
     def _group_by_sale(self):
         res = super()._group_by_sale()
-        res += ',l.retribution_cost'
+        res += ',l.retribution_cost,s.state_specific'
         return res
