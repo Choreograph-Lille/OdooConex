368828e90d771eb5d7ee4cf3de0e37357f957024
a.ramamonjisoa <dandriantsilavo@gmail.com>
2023-05-26 14:54:47 +0300

    [fix] HT01302: updates on condition/exclusion task management

--- choreograph-addons/choreograph_sale_project/models/sale_order.py
+++ choreograph-addons/choreograph_sale_project/models/sale_order.py
@@ -499,13 +499,14 @@ class SaleOrder(models.Model):
     def action_create_task_from_condition(self):
         super().action_create_task_from_condition()
         project_id = self.project_ids[0] if self.project_ids else False
-        if project_id and project_id.stage_id == self.env.ref('choreograph_project.planning_project_stage_planified'):
+        if project_id and project_id.stage_id != self.env.ref('choreograph_project.planning_project_stage_draft'):
             task_draft_stage_id = self.env.ref('choreograph_project.project_task_type_draft')
             for condition_id in self.operation_condition_ids.filtered(lambda item: item.task_id.stage_id == task_draft_stage_id):
-                if condition_id.task_id.task_number in ('5', '10', '15'):
-                    project_id._update_task_stage(condition_id.task_id.task_number, WAITING_FILE_TASK_STAGE)
-                elif condition_id.task_id.task_number in ('20', '25', '35'):
-                    project_id._update_task_stage(condition_id.task_id.task_number, TODO_TASK_STAGE)
+                condition_id.task_id.update_task_stage(WAITING_FILE_TASK_STAGE)
+                # if condition_id.task_id.task_number in ('5', '10', '15'):
+                #     project_id._update_task_stage(condition_id.task_id.task_number, WAITING_FILE_TASK_STAGE)
+                # elif condition_id.task_id.task_number in ('20', '25', '35'):
+                #     project_id._update_task_stage(condition_id.task_id.task_number, TODO_TASK_STAGE)
 
     @api.depends('project_ids')
     def compute_operation_code(self):
