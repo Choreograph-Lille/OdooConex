a658d20544edc87ae1cc7991dfa4d13d0c6664c3
Zo Herivahatra <zoherivahtra@gmail.com>
2023-07-25 14:54:51 +0300

    [IMPL] HT02041 Gao & MyModel URL [FIX] HT02162 fiscal position [FIX] HT02145 Invoice email from

---
+++ choreograph-addons/choreograph_base/models/mail_compose_message.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+
+
+class MailComposeMessage(models.TransientModel):
+	_inherit = "mail.compose.message"
+
+	def _action_send_mail(self, auto_commit=False):
+		user_url = False
+		if self.template_id and self.template_id.user_url:
+			user_url = self.template_id.user_url
+		else:
+			partners = self.partner_ids
+			users = self.env['res.users'].sudo().search([('partner_id', 'in', partners.ids)])
+			if any(user.is_standard() or user.is_validator() for user in users):
+				user_url = 'mymodel'
+		return super(MailComposeMessage, self.with_context(user_url=user_url))._action_send_mail(auto_commit)
+
