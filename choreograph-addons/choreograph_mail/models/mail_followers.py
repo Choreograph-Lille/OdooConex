3f78e9b15bc58e29c420451a0c61e80fd232ec8b
Omar OUHARI <omar.ouhari@gmail.com>
2023-08-22 15:18:31 +0200

    [FIX] #HT01427: improve codes + flake8

--- choreograph-addons/choreograph_mail/models/mail_followers.py
+++ choreograph-addons/choreograph_mail/models/mail_followers.py
@@ -6,24 +6,16 @@ from odoo import api, models
 class MailFollowers(models.Model):
     _inherit = 'mail.followers'
 
-    @staticmethod
-    def check_field_followers(source_id, field_name):
-        model_name = {
-            'partner_id': 'res.partner',
-            'user_ids': 'res.users'
-        }
-        return source_id and field_name in source_id and source_id[field_name]._name == model_name[field_name] and source_id[field_name]
-
     @api.model_create_multi
     def create(self, vals_list):
         if not self.env.company.disable_followers:
             return super(MailFollowers, self).create(vals_list)
-        tmp_vals_list = self.check_partner_group(vals_list)
+        tmp_vals_list = self._check_is_internal_followers(vals_list)
         res = super(MailFollowers, self).create(tmp_vals_list)
         res._invalidate_documents(tmp_vals_list)
         return res
 
-    def check_partner_group(self, vals):
+    def _check_is_internal_followers(self, vals):
         """
         Check if the user related to the created record's partner_id exists and is an internal user
         If not we don't create the follower
@@ -32,9 +24,11 @@ class MailFollowers(models.Model):
         """
         values = copy.deepcopy(vals)
         if not self.env.context.get('manual_follower_add', False):
+            partner_obj = self.env['res.partner']
             for value in vals:
                 if value.get('partner_id'):
-                    user = self.env['res.users'].search([('partner_id', '=', value['partner_id'])], limit=1)
-                    if not user or not user.has_group('base.group_user'):
-                        values.remove(value)
+                    users = partner_obj.browse(value['partner_id']).user_ids
+                    if users and users[0].has_group('base.group_user'):
+                        continue
+                    values.remove(value)
         return values
