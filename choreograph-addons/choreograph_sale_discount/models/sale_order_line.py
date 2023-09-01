02dab914a29c38dc2b6f408104ea481bf6b9aff1
Zo Herivahatra <zoherivahtra@gmail.com>
2023-09-01 14:03:43 +0300

    [FIX] HT02396 Fix keep discount name on editing quantity, price etc

--- choreograph-addons/choreograph_sale_discount/models/sale_order_line.py
+++ choreograph-addons/choreograph_sale_discount/models/sale_order_line.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import re
 from odoo import fields, models, api, _
 from odoo.tools.misc import formatLang
 
@@ -21,6 +21,7 @@ class SaleOrderLine(models.Model):
 		ondelete='cascade',
 		help='Discount notes related to this product lines'
 	)
+	discount_pricelist_id = fields.Many2one('product.pricelist', help="Used only to manage the discount line section name")
 
 	@api.depends('product_id', 'price_unit', 'discount', 'product_uom_qty', 'order_id.pricelist_id')
 	def _compute_discount_line(self):
@@ -62,19 +63,25 @@ class SaleOrderLine(models.Model):
 								'name': display_discount,
 								'sequence': line.sequence,
 								'discount_source_line_id': line.id,
+								'discount_pricelist_id': order.pricelist_id.id,
 							}
 						)
 					]
 				else:
+					if order.pricelist_id != line.discount_line_ids[0].discount_pricelist_id:
+						new_discount_names = display_discount
+					else:
+						new_discount_names = self.update_discount_name(order, line.discount_line_ids[0].name, discount_value)
 					line.discount_line_ids = [
 						(
 							1, line.discount_line_ids[0].id,
 							{
 								'order_id': order.id,
 								'display_type': 'line_note',
-								'name': display_discount,
+								'name': new_discount_names,
 								'sequence': line.sequence,
-								'discount_source_line_id': line.id
+								'discount_source_line_id': line.id,
+								'discount_pricelist_id': order.pricelist_id.id,
 							}
 						)
 					]
@@ -82,6 +89,16 @@ class SaleOrderLine(models.Model):
 				discount_line = line.discount_line_ids and line.discount_line_ids[0]
 				if discount_line:
 					order.order_line = [(2, discount_line.id)]
+
+	def update_discount_name(self, order, name, discount_values):
+		pattern = r'\b\d+(\,\d+)?%\s\d+\,\d+\b\s'
+		discount_names = name.split('\n')
+		new_discount_names = []
+		for index, name in enumerate(discount_names):
+			p, d = discount_values[index]
+			d = d.replace(order.currency_id.symbol, '')
+			new_discount_names.append(re.sub(pattern, f'{int(float(p))}%' + ' ' + str(d), name))
+		return '\n'.join(new_discount_names)
 
 	@staticmethod
 	def get_discount(p):
