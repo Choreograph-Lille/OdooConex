3db5cd516b238f928da7ff6861b294203d81d324
Omar OUHARI <omar.ouhari@gmail.com>
2023-09-25 09:52:03 +0200

    Revert "[FIX] HT02405 fix base url inside email"
    This reverts commit 4a8e890215c224d3e375d339af9122320b657270.

--- choreograph-addons/choreograph_base/models/base.py
+++ choreograph-addons/choreograph_base/models/base.py
@@ -6,6 +6,6 @@ class BaseModel(models.AbstractModel):
     _inherit = 'base'
 
     def get_base_url(self):
-        if self._context.get('web_mymodel_url'):
-            return self.env['ir.config_parameter'].sudo().get_param('web.mymodel.url')
+        if self._context.get('user_url', False) == 'gao':
+            return self.env['ir.config_parameter'].sudo().get_param('web.gao.url')
         return super().get_base_url()
