195d1edccad00e59073af14cc19e646c0c6ae938
David Andriantsilavo <dandriantsilavo@gmail.com>
2024-05-03 15:59:02 +0300

    [add] HT04596: add file generation method

--- choreograph-addons/choreograph_sage/models/__init__.py
+++ choreograph-addons/choreograph_sage/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from . import choreograph_sage_ftp_server
 from . import res_partner
 from . import account_move
 from . import account_tax
-from . import choreograph_sage_ftp_server
--- choreograph-addons/choreograph_sage/models/account_move.py
+++ choreograph-addons/choreograph_sage/models/account_move.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+import base64
+import contextlib
+import io
+from odoo.exceptions import ValidationError
 from odoo.addons.choreograph_sage.models.res_partner import PAYMENT_CHOICE
 
 
@@ -28,14 +32,37 @@ class AccountMove(models.Model):
         self.payment_choice = self.partner_id.payment_choice
 
     def generate_sage_file(self, move_type, limit=None):
+        ftp_server = self.env['choreograph.sage.ftp.server'].search([], limit=1)
+        if not ftp_server:
+            raise ValidationError(_('Make sure to configure an active FTP server!'))
+
         if move_type == 'in':
             move_types = ['in_invoice', 'in_refund']
         elif move_type == 'out':
             move_types = ['out_invoice', 'out_refund']
 
-        move_ids = self.env['account.move'].search([('move_type', 'in', move_types), ('is_transferred_to_sage', '=', False)], limit=limit)
-        if move_ids:
-            # TODO: generate the file and send to the FTP server
-            move_ids.write({
+        moves = self.env['account.move'].search([('move_type', 'in', move_types), ('state', '=', 'posted'),
+                                                 ('is_transferred_to_sage', '=', False)], limit=limit)
+        if moves:
+            file = self.create_sage_file(moves)
+            # TODO: send the file to the the server and create LOG
+            moves.write({
                 'is_transferred_to_sage': True,
             })
+
+    def create_sage_file(self, moves):
+        """
+        Create the binary file
+        :param moves: account_move
+        :return: return the binary file
+        """
+        with contextlib.closing(io.BytesIO()) as buf:
+            wrapper = io.TextIOWrapper(
+                buf,
+                encoding='utf-8',
+                write_through=True,
+            )
+            # TODO: get the fields values from moves
+            wrapper.write('TEST SAGE FILE' + '\n')
+            out = base64.encodebytes(buf.getvalue())
+        return out
