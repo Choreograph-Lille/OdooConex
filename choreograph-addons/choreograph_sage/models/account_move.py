92a3203f48c2526435b707ef5d00cf442d292a7c
Tsiry BARINESA <t.barinesa@etechconsulting-mg.com>
2024-05-15 15:37:22 +0300

    [IMP] HT04596 Update choreograph.sage.ftp.server and add SFTP transfert

--- choreograph-addons/choreograph_sage/models/account_move.py
+++ choreograph-addons/choreograph_sage/models/account_move.py
@@ -2,10 +2,12 @@
 
 from odoo import models, fields, api, _
 import base64
-import contextlib
-import io
+import csv
+from datetime import datetime
+import paramiko
 from odoo.exceptions import ValidationError
 from odoo.addons.choreograph_sage.models.res_partner import PAYMENT_CHOICE
+
 
 
 class AccountMove(models.Model):
@@ -32,7 +34,7 @@ class AccountMove(models.Model):
         self.payment_choice = self.partner_id.payment_choice
 
     def generate_sage_file(self, move_type, limit=None):
-        ftp_server = self.env['choreograph.sage.ftp.server'].search([], limit=1)
+        ftp_server = self.env['choreograph.sage.ftp.server'].search([('active','=',True)], limit=1)
         if not ftp_server:
             raise ValidationError(_('Make sure to configure an active FTP server!'))
 
@@ -44,25 +46,116 @@ class AccountMove(models.Model):
         moves = self.env['account.move'].search([('move_type', 'in', move_types), ('state', '=', 'posted'),
                                                  ('is_transferred_to_sage', '=', False)], limit=limit)
         if moves:
-            file = self.create_sage_file(moves)
+            self.create_sage_file(moves, ftp_server)
+            
             # TODO: send the file to the the server and create LOG
             moves.write({
                 'is_transferred_to_sage': True,
             })
 
-    def create_sage_file(self, moves):
+    def prepare_file_rows(self, moves):
+        """
+        Prepare rows value ofr the binary file
+        :param moves: account_move
+        :return: Array
+        """
+        fields = [
+            "Code Société",
+            "Journal",
+            "Type de Pièce",
+            "Compte général",
+            "Rôle Tiers",
+            "date piece",
+            "date échéance",
+            "Mode reglement",
+            "Référence Pièce",
+            "Libellé",
+            "Devise",
+            "Débit devise",
+            "Crédit Devise",
+            "Débit EUR",
+            "Crédit EUR",
+            "Libellé CARTESIS",
+            "codeCARTESIS",
+            "Département",
+            "Média",
+            "ProfilTVA",
+            "NUMDEVIS",
+            "JOUMM",
+            "IDODOO"
+        ]
+        rows = []
+        
+        type = moves[-1].move_type
+                
+        for line in moves.line_ids:
+            if type == 'in':
+                role = line.move_id.partner_id.third_party_role_supplier_code
+            else:
+                role = line.move_id.partner_id.third_party_role_client_code
+            
+            vals= {
+                "Code Société":"",
+                "Journal": 'ACH' if type == 'in' else 'VTE',
+                "Type de Pièce":'FF' if type == 'in' else 'FC',
+                "Compte général":line.account_id.code or "",
+                "Rôle Tiers":role or "",
+                "date piece":line.move_id.invoice_date or "",
+                "date échéance":line.move_id.invoice_date_due or "",
+                "Mode reglement":line.move_id.payment_choice or "",
+                "Référence Pièce":line.move_id.name or "",
+                "Libellé":line.move_id.wording or "",
+                "Devise":line.move_id.currency_id.name or "",
+                "Débit devise":line.debit,
+                "Crédit Devise":line.credit,
+                "Débit EUR":"",
+                "Crédit EUR":"",
+                "Libellé CARTESIS":"",
+                "codeCARTESIS":line.move_id.partner_id.cartesis_code or "",
+                "Département":"",
+                "Média":"",
+                "ProfilTVA":','.join(line.tax_ids.filtered(lambda l:l.tva_profile_code !=False).mapped("tva_profile_code")),
+                "NUMDEVIS":line.move_id.invoice_origin or "",
+                "JOUMM":"",
+                "IDODOO":line.id
+            }
+            rows.append(vals)
+        return fields, rows
+
+    def create_sage_file(self, moves, ftp_server):
         """
         Create the binary file
         :param moves: account_move
-        :return: return the binary file
-        """
-        with contextlib.closing(io.BytesIO()) as buf:
-            wrapper = io.TextIOWrapper(
-                buf,
-                encoding='utf-8',
-                write_through=True,
-            )
-            # TODO: get the fields values from moves
-            wrapper.write('TEST SAGE FILE' + '\n')
-            out = base64.encodebytes(buf.getvalue())
-        return out
+        :return: Boolean
+        """       
+        # Establish SSH connection
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        #Get path of key as attachment
+        key_path = ftp_server.key_attachment_id._full_path(ftp_server.key_attachment_id.store_fname)
+        passphrase = ftp_server.passphrase
+        fields, rows = self.prepare_file_rows(moves)
+        
+        file_name = f"{datetime.now().strftime('%Y-%m-%d %H:%M')}.csv"
+        try:
+            key = paramiko.RSAKey.from_private_key_file(key_path, password=passphrase)
+            
+            ssh_client.connect(ftp_server.host, ftp_server.port, ftp_server.username, pkey=key)
+            # Create and Transfert file
+            with paramiko.SFTPClient.from_transport(ssh_client.get_transport()) as sftp:
+                # Create temporary file
+                with open(file_name, 'w', newline='') as temp_file:
+                    writer = csv.DictWriter(temp_file, delimiter='\t',fieldnames = fields)                    
+                    writer.writeheader()
+                    writer.writerows(rows)
+                # Copy temporary file into server
+                sftp.put(file_name, f"{ftp_server.path}/{file_name}")
+                
+        except Exception as e:
+            pass
+                
+        finally:
+            ssh_client.close()
+
+          
+        return True
