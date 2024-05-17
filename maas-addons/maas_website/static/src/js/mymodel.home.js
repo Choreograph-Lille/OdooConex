cea14095d5bb38be591bd22ce4fa41e3ad339b71
David Andriantsilavo <dandriantsilavo@gmail.com>
2024-05-17 15:14:14 +0300

    [fix] HT04075: fix pbi filter dynamisation

--- maas-addons/maas_website/static/src/js/mymodel.home.js
+++ maas-addons/maas_website/static/src/js/mymodel.home.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     function get_home_report_bi(){
         $.ajax({
             dataType: 'json',
-            url: '/report/' + parseInt($('#partner_id').value) + '/res_partner',
+            url: '/report/' + parseInt($('#partner_id').val()) + '/res_partner',
             type: 'POST',
             proccessData: false,
             data: {},
@@ -12,8 +12,7 @@ $(document).ready(function(){
                 a.push({id: b['id'], report_bi_src: b['report_bi_src'],});
                 return a;
             }, []);
-            var iframe = document.getElementById('home_report_bi_src');
-            $(iframe).attr('srcdoc', data[0]['report_bi_src']);
+            $('#home_report_bi_src').attr('srcdoc', data[0]['report_bi_src']);
         });
     }
 })
