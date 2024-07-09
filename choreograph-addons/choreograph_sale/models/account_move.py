7a7f79e77b99931999809a45dc43039c954b0317
tsiry-odoo-etech <t.barinesa@etechconsulting-mg.com>
2024-07-09 16:24:59 +0300

    [IMP] HT02497 Change invoice terms lang relative to currency

--- choreograph-addons/choreograph_sale/models/account_move.py
+++ choreograph-addons/choreograph_sale/models/account_move.py
@@ -7,7 +7,19 @@ from odoo.tools import is_html_empty
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    @api.depends('move_type', 'partner_id', 'company_id')
+    def _get_lang_and_currency(self):  
+        if self.env.ref('base.EUR') and self.currency_id == self.env.ref('base.EUR'):
+            lang = 'fr_FR'
+            currency_id = self.env.ref('base.EUR')
+        elif self.env.ref('base.GBP') and self.currency_id == self.env.ref('base.GBP'):
+            lang = 'en_GB'
+            currency_id = self.env.ref('base.GBP')
+        else:
+            lang = self.partner_id.lang or self.env.user.lang
+            currency_id = False
+        return lang, currency_id
+    
+    @api.depends('move_type', 'partner_id', 'company_id', 'currency_id')
     def _compute_narration(self):
         """
         Same as native, but instead use 'invoice.terms.conditions' and 'invoice_terms_c9h'
@@ -20,10 +32,12 @@ class AccountMove(models.Model):
             if not use_invoice_terms:
                 move.narration = False
             else:
-                lang = move.partner_id.lang or self.env.user.lang
+                lang, currency_id = move._get_lang_and_currency()
                 if not move.company_id.terms_type == 'html':
-                    narration = move.company_id.with_context(lang=lang).invoice_terms_c9h if not is_html_empty(
-                        move.company_id.invoice_terms_c9h) else ''
+                    narration = move.company_id.with_context(lang=lang).invoice_terms_c9h if (
+                        not is_html_empty(move.company_id.invoice_terms_c9h) and 
+                        currency_id
+                    ) else ''
                 else:
                     baseurl = self.env.company.get_base_url() + '/terms'
                     context = {'lang': lang}
