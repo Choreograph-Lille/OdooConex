626902ea34a85ac7d890a51a71cd31b65f6a2118
tsiry-odoo-etech <t.barinesa@etechconsulting-mg.com>
2024-11-18 14:21:44 +0300

    [FIX] HT07406 Add condition for report_bi_src in data

--- maas-addons/maas_website/static/src/js/mymodel.home.js
+++ maas-addons/maas_website/static/src/js/mymodel.home.js
@@ -12,8 +12,9 @@ $(document).ready(function(){
                 a.push({id: b['id'], report_bi_src: b['report_bi_src'],});
                 return a;
             }, []);
-            console.log(data)
-            $('#home_report_bi_src').attr('srcdoc', data[0]['report_bi_src']);
+            if (data.length == 0){
+                $('#home_report_bi_src').attr('srcdoc', data[0]['report_bi_src']);
+            } 
         });
     }
 })
