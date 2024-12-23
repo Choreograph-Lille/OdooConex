1126d8d530854e5dfee5ebd97b493e4f4b3a3a05
tsiry-odoo-etech <t.barinesa@etechconsulting-mg.com>
2024-12-23 16:26:38 +0300

    [IMP] HT07476 Override _reverse_moves function to disable _post method call

--- choreograph-addons/choreograph_account/models/account_move.py
+++ choreograph-addons/choreograph_account/models/account_move.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
+from odoo import models, fields, api, _, Command
+TYPE_REVERSE_MAP = {
+    'entry': 'entry',
+    'out_invoice': 'out_refund',
+    'out_refund': 'entry',
+    'in_invoice': 'in_refund',
+    'in_refund': 'entry',
+    'out_receipt': 'out_refund',
+    'in_receipt': 'in_refund',
+}
 
 
 class AccountMove(models.Model):
@@ -32,3 +40,40 @@ class AccountMove(models.Model):
         if self.partner_id.child_ids:
             adresses |= self.partner_id.child_ids.filtered(lambda rp: rp.type == 'invoice')
         return ','.join([str(rp.id) for rp in adresses])
+
+    def _reverse_moves(self, default_values_list=None, cancel=False):
+        ''' Override _reverse_moves function
+        :return: An account.move recordset, reverse of the current self without posted.
+        '''
+        if not default_values_list:
+            default_values_list = [{} for move in self]
+
+        if cancel:
+            lines = self.mapped('line_ids')
+            # Avoid maximum recursion depth.
+            if lines:
+                lines.remove_move_reconcile()
+
+        reverse_moves = self.env['account.move']
+        for move, default_values in zip(self, default_values_list):
+            default_values.update({
+                'move_type': TYPE_REVERSE_MAP[move.move_type],
+                'reversed_entry_id': move.id,
+                'partner_id': move.partner_id.id,
+            })
+            reverse_moves += move.with_context(
+                move_reverse_cancel=cancel,
+                include_business_fields=True,
+                skip_invoice_sync=move.move_type == 'entry',
+            ).copy(default_values)
+
+        reverse_moves.with_context(skip_invoice_sync=cancel).write({'line_ids': [
+            Command.update(line.id, {
+                'balance': -line.balance,
+                'amount_currency': -line.amount_currency,
+            })
+            for line in reverse_moves.line_ids
+            if line.move_id.move_type == 'entry' or line.display_type == 'cogs'
+        ]})
+        
+        return reverse_moves
