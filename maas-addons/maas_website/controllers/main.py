82d89caa788ea34f0b17acc23e111a30edca4257
tsiry-odoo-etech <t.barinesa@etechconsulting-mg.com>
2025-01-20 12:09:42 +0300

    [DEL] HT07309 Delete CDN script and style

--- maas-addons/maas_website/controllers/main.py
+++ maas-addons/maas_website/controllers/main.py
@@ -386,10 +386,9 @@ class OperationWebsite(http.Controller):
 <!DOCTYPE html>
 <head>
     <meta name="viewport" content="width=device-width">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; object-src 'self'; img-src 'self'; connect-src 'self'; script-src-elem 'self' https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js;">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; object-src 'self'; img-src 'self'; connect-src 'self';">
     <meta charset="utf-8">
     <title>Power BI embedded - Conexance</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
     <script src="/maas_website/static/src/report/es6-promise.js" integrity="sha384-o56RdTPUrpuJrwU5s8XdhI8EJ1rlkBUrHZUY7U4ykFo7mkS4c/H/qXMIRyRM86mz" crossorigin="anonymous"></script>
     <script src="/maas_website/static/src/report/powerbi.js" integrity="sha384-Sxdglzx/DupIgv9dbj82EYxklXXeizMUTc+rqKbjwQtKD5MLpVzUefmSClYOtMgT" crossorigin="anonymous"></script>
 </head>
