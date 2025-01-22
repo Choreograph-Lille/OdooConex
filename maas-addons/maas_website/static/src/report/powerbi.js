5814822b7aedf0246a7ec050c05518ed7656921c
tsiry-odoo-etech <139131968+tsiry-odoo-etech@users.noreply.github.com>
2025-01-22 14:50:00 +0300

    Revert "[ADD] HT07309 Add commnet for testing"

--- maas-addons/maas_website/static/src/report/powerbi.js
+++ maas-addons/maas_website/static/src/report/powerbi.js
@@ -1,5 +1,4 @@
 /*! powerbi-client v2.5.2 | (c) 2016 Microsoft Corporation MIT */
-/*! Comment add here for test*/
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
