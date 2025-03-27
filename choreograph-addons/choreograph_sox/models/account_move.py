0cb66e6fb6a118cac244bd4e15a154e7dd052506
tsiry-odoo-etech <t.barinesa@etechconsulting-mg.com>
2025-03-27 09:56:49 +0300

    [IMP] HT07865 Add confidential role

--- choreograph-addons/choreograph_sox/models/account_move.py
+++ choreograph-addons/choreograph_sox/models/account_move.py
@@ -65,11 +65,7 @@ class AccountMove(models.Model):
                 )
             )
         ):
-            if (
-                not self.env.user.has_group("choreograph_sox.group_purchasing_preparer_profile_res_groups") and
-                not self.env.user.has_group("choreograph_sox.group_validator_1_purchase_profile_res_groups") and
-                not self.env.user.has_group("choreograph_sox.group_validator_2_purchase_profile_res_groups")
-            ):
+            if not self.env.user.has_group("choreograph_sox.group_confidential_profile_res_groups"):
                 args = expression.AND([[('is_confidential', '=', False)], args])
         
         return super(AccountMove, self).search(args, offset=offset, limit=limit, order=order, count=count)
