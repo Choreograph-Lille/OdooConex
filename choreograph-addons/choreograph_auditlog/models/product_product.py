acfe2fc28e53a2a7dd45e5e6fc759b0da6e7aa8e
tsiry <t.barinesa@etechconsulting-mg.com>
2025-10-15 18:11:55 +0300

    [IMP] HT09112 :  Add purchase retribution report

--- choreograph-addons/choreograph_auditlog/models/product_product.py
+++ choreograph-addons/choreograph_auditlog/models/product_product.py
@@ -5,4 +5,3 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     retribution = fields.Boolean(related='product_tmpl_id.retribution',store=True)
-    mymodel = fields.Boolean(related='product_tmpl_id.mymodel',store=True)
--- choreograph-addons/choreograph_auditlog/models/product_template.py
+++ choreograph-addons/choreograph_auditlog/models/product_template.py
@@ -5,4 +5,3 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     retribution = fields.Boolean(default=False)
-    mymodel = fields.Boolean(default=False)
