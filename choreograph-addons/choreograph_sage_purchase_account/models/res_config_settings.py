2ccc9883b841935d78a6e5adbf750974ab9f8dfb
Brice-Odoo-Etech <b.raharijaona@etechconsulting-mg.com>
2026-07-16 10:32:49 +0300

    Update translation
    (cherry picked from commit cb01ae255041adeb7ff13c4f8be702ffed98db0c)

--- choreograph-addons/choreograph_sage_purchase_account/models/res_config_settings.py
+++ choreograph-addons/choreograph_sage_purchase_account/models/res_config_settings.py
@@ -8,6 +8,6 @@ class ResConfigSettings(models.TransientModel):
 
     sage_file_prefix = fields.Char(string="Prefix", config_parameter='choreograph_sage_purchase_account.prefix')
     sage_file_suffix = fields.Char(string="Suffix", config_parameter='choreograph_sage_purchase_account.suffix')
-    sage_file_date = fields.Datetime(string="Date", config_parameter='choreograph_sage_purchase_account.sage_file_date')
+    sage_file_date = fields.Datetime(string="Date import", config_parameter='choreograph_sage_purchase_account.sage_file_date')
     sage_purchase_default_journal_id = fields.Many2one('account.journal',
                                                        config_parameter='choreograph_sage_purchase_account.purchase_default_journal_id')
