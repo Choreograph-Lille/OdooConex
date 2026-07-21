2dd84ab689a6df161c47f2b64674abb6cac631ec
Brice-Odoo-Etech <b.raharijaona@etechconsulting-mg.com>
2026-07-21 15:48:05 +0300

    [UPD] Refactor sage_purchase_account and add new module choreograph_sage_sftp_import
    (cherry picked from commit b35ef7fc6264e0d6a146601586a2fc7dac821f65)

--- choreograph-addons/choreograph_sage_sftp_import/__manifest__.py
+++ choreograph-addons/choreograph_sage_sftp_import/__manifest__.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Choreograph SAGE Purchase Account',
+    'name': 'Choreograph SAGE SFTP Import',
     'version': '16.0.0.0',
     'license': 'LGPL-3',
     'category': 'Accounting',
     'sequence': -98,
     'summary': 'Manage Choreograph SAGE In invoice import',
-    'description': """This module allows to import purchase invoice from sage""",
+    'description': """This module allows to import invoice from sage""",
     'author': 'ArkeUp',
     'website': 'https://arkeup.com',
     'depends': [
         'choreograph_sage_sftp_server',
-        'choreograph_sage_sftp_import',
         'choreograph_account',
         'account'
     ],
     'data': [
-        'data/ir_cron_data.xml',
-        'views/res_config_settings_views.xml'
     ],
     'assets': {
         'web._assets_primary_variables': [],
